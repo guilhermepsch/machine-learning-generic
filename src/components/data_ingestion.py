fa1476aa47d10c497ac87a60ac3022bd7fc600e0
Guilherme Schneidt <guilhermepsch@gmail.com>
2025-03-02 13:38:25 -0300

    feat: model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -31,7 +32,7 @@ class DataIngestion:
       train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
       test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
       
-      logging.info("INgestion of the data is completed")
+      logging.info("Ingestion of the data is completed")
       
       return (
         self.ingestion_config.train_data_path,
@@ -45,4 +46,11 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    
+    
+    
+    
