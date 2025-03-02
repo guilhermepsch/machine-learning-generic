93d220e01bec77cbd9d5fe215b12cdb6a7ec7532
Guilherme Schneidt <guilhermepsch@gmail.com>
2025-03-01 22:31:27 -0300

    feat: data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -42,3 +43,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
